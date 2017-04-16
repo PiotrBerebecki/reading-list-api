400999bff4cc0514f4ddd880260deadccb6ac129
PiotrBerebecki <petebere@gmail.com>
2017-04-16 06:52:42 +0100

    Add redux packages

---
+++ src/database.js
@@ -0,0 +1,65 @@
+module.exports = {
+  '1': {
+    id: 1,
+    tags: ['react'],
+    title: 'React ComponentDidMount',
+    body: 'About React Lifecycle Methods: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://raw.githubusercontent.com/hex13/atom-lupa/master/screenshot-main.jpg'
+  },
+  '2': {
+    id: 2,
+    tags: ['react', 'javascript'],
+    title: 'React - A JavaScript Library',
+    body: 'This article has react and javascript tags. Hello React is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://subhadipsblog.files.wordpress.com/2016/10/resources-to-get-you-started-with-reactjs.jpg'
+  },
+  '3': {
+    id: 3,
+    tags: ['react'],
+    title: 'React Conf',
+    body: 'Conf Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'http://codewinds.com/assets/article/reactjs-conf-panel-dsc_5276-800.jpg'
+  },
+  '4': {
+    id: 4,
+    tags: ['javascript'],
+    title: 'Javascript30',
+    body: 'JS30 React Lifecycle Methods: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://cdn-images-1.medium.com/max/800/1*C5JB4zEpTQgt-VZjUodRDA.png'
+  },
+  '5': {
+    id: 5,
+    tags: ['javascript'],
+    title: 'JS training by Kyle Simpson',
+    body: 'About Kyle and Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://embedwistia-a.akamaihd.net/deliveries/75735f374ae45f221eed1b6cdb6839648af81c0b.jpg'
+  },
+  '6': {
+    id: 6,
+    tags: ['javascript'],
+    title: 'JS Graphic',
+    body: 'Graphic and Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://cdn-images-1.medium.com/max/750/1*PkPwdnx5-wxxdwEnxMNnXA.jpeg'
+  },
+  '7': {
+    id: 7,
+    tags: ['css'],
+    title: 'All about CSS elements',
+    body: 'CSS ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'http://materialdesignblog.com/wp-content/uploads/2015/08/Android-Material-Design-UI-Kit-Download-Free-PSD-Featured.jpg'
+  },
+  '8': {
+    id: 8,
+    tags: ['css', 'javascript'],
+    title: 'CSS and JavaScript',
+    body: 'CSS and JS ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://www.smashingmagazine.com/wp-content/uploads/2011/10/sm-wp-css-js.jpg'
+  },
+  '9': {
+    id: 9,
+    tags: ['css', 'react'],
+    title: 'CSS in React with Styled Components',
+    body: 'Styled Components ipsum dolor sit amet, consectetur adipisicing elit. Aliquid doloremque nesciunt nobis voluptas. Deserunt optio vero pariatur quidem commodi laudantium dolore eligendi. Aliquam impedit soluta obcaecati aut. Ab, facilis, molestiae.',
+    image_url: 'https://camo.githubusercontent.com/62d88c898d078e91a7c29bd45636315b02598106/687474703a2f2f696d6775722e636f6d2f6b3768343563332e6a7067'
+  },
+};
